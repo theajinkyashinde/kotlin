cb58b1f7d449acb0e1e3794db02031109788ad99
Ajinkya Shinde <ajinkya.shinde@hotmail.com>
2024-10-22 17:03:44 +0200

    Implement Primary Constructor

--- src/kotlin_oop_concepts/constructor/2_primary_constructor.kt
+++ src/kotlin_oop_concepts/constructor/2_primary_constructor.kt
@@ -1,2 +1,35 @@
 package kotlin_oop_concepts.constructor
 
+/*
+Primary Constructor: Please see constructor definition in 1_default_constructor file.
+Also, primary constructor always takes place in round brackets after class name, and it contains
+properties or parameters.
+
+It can't contain any runnable code.
+Runnable code can write into init block or body of the class
+
+ */
+
+/*
+
+In this class name and age are the properties of the class, and declared it in primary
+c'tor. Whereas, canVote is parameter like normal function, which can be used further in evaluated expression.
+
+ */
+class PrimaryPerson (val name : String, val age: Int, canVote: Boolean) {
+
+    val primaryPersonCanVote = canVote
+
+    fun primaryPersonName() {
+        println("Name : $name")
+    }
+
+    fun primaryPersonCanVote() {
+        println("Can primary person vote? : $primaryPersonCanVote")
+    }
+
+}
+
+fun main () {
+    val primaryPerson = PrimaryPerson("Ajinkya", 36, true)
+}
